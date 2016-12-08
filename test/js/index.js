0f38daed52890482815c8ca9cb71999bfa40ee0e
zhongyufei <zhongyufei@didichuxing.com>
2016-12-08 14:54:29 +0800

    update test

--- test/js/index.js
+++ test/js/index.js
@@ -17,18 +17,32 @@ let App = {
   init () {
     this.render()
 
-    geo.register()
-
+    geo.register({
+      type: 'ali',
+      app_key: 'mykey',
+      app_name: 'myappname'
+    })
     geo.getCurrentPosition('ali')
     .then(pos => {
       let html = `<p>lat: <span>${pos.lat}</span></p><p>lng: <span>${pos.lng}</span></p><p>maptype: <span>${pos.maptype}</span></p>`
       document.getElementById('amap').innerHTML = html
     })
 
+    geo.register({
+      type: 'baidu',
+      app_key: 'mykey',
+      app_name: 'myappname'
+    })
     geo.getCurrentPosition('baidu')
     .then(pos => {
       let html = `<p>lat: <span>${pos.lat}</span></p><p>lng: <span>${pos.lng}</span></p><p>maptype: <span>${pos.maptype}</span></p>`
       document.getElementById('bmap').innerHTML = html
+    })
+
+    geo.register({
+      type: 'qq',
+      app_key: 'OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77',
+      app_name: 'myappname'
     })
     geo.getCurrentPosition('qq')
     .then(pos => {
@@ -36,12 +50,6 @@ let App = {
       let html = `<p>lat: <span>${pos.lat}</span></p><p>lng: <span>${pos.lng}</span></p><p>maptype: <span>${pos.maptype}</span></p>`
       document.getElementById('qqmap').innerHTML = html
     })
-    setTimeout(() => {
-      geo.getCurrentPosition('qq')
-      .then(pos => {
-        console.log(2, pos)
-      })
-    }, 5000)
     geo.getCurrentPosition('h5')
     .then(pos => {
       let html = `<p>lat: <span>${pos.lat}</span></p><p>lng: <span>${pos.lng}</span></p><p>maptype: <span>${pos.maptype}</span></p>`
@@ -49,7 +57,7 @@ let App = {
     })
   },
 
-  render (data) {
+  render () {
     let me = this
     me.$el.html(me.appTemplate())
   }
